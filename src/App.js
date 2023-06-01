10046181412fedbe03fbf5c209dc826625fa5d13
eunjikwak <eje11@naver.com>
2023-06-01 14:48:25 +0900

    라우터 라이브러리 추가후 경로 지정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
-import LoginBG from './pages/login/loginBG';
-
+import LoginBG from './pages/login/loginBG'
+import Main from './pages/main/Main';
+import ExhibitListPage from './pages/ExhibitListPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <LoginBG/>
-      ddd
-    </div>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/login" element={<LoginBG/>}/>
+        <Route path="/exhibitList" element={<ExhibitListPage/>}/>
+      </Routes>
+      </Router>
+      
   );
 }
 
---
+++ src/util/modal/ExhibitModal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const ExhibitModal = () => {
+
+
+    return(
+
+        <>
+        <div>이미지영역</div>
+        <div>
+            
+
+        </div>
+        </>
+    );
+}
+
+export default ExhibitModal;

fd08547db121ef52f0c0e38035761046fc90f2d7
eunjikwak <eje11@naver.com>
2023-06-01 14:19:52 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import LoginBG from './pages/myPage/loginBG';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-  
-=======
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <LoginBG/>
->>>>>>> dde7f31cd781210cd58b23594540b620e0855430
     </div>
   );
 }
---
+++ src/pages/ExhibitListPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+const ExhibitListPage = () => {
+
+    return(
+        <>
+        <div className="header">해더</div>
+        <div>전시 공공 API 보여질 영역</div>
+        <div>카테고리</div>
+        <div>
+            보여질 박스
+            <div>셀렉트 박스</div>
+            <div>전시 정보 컴포넌트</div>
+            <div>페이지 네이션</div>
+        </div>
+        </>
+    );
+}
+
+export default ExhibitListPage

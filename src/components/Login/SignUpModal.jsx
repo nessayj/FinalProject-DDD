243ccacf78838af835cac4d09ee1312ec8e916d0
taek <miloe0117@gmail.com>
2023-06-01 23:03:25 +0900

    회원가입페이지 - 유효성검사 추가

--- src/components/Login/SignUpModal.jsx
+++ src/components/Login/SignUpModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -6,13 +6,14 @@ const Container = styled.div`
     top: 0;
     width: 100vw;
     height: 100vh;
+    box-sizing: border-box;
     /* background-color: aqua; */
     
 
 `;
 const Modal = styled.div`
     width: 40vw;
-    min-width: 350px;
+    min-width: 600px;
     height: 70vh;
     background-color: white;
     position: absolute;
@@ -23,13 +24,11 @@ const Modal = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    
         .title{
             font-size: 1.5rem;
             font-weight: bold;
-            margin-top: 3rem;
             text-align: center;
+            margin :3rem 0 0.2rem 0 ;
         }
         .wrap{
             width: 100%;
@@ -41,50 +40,66 @@ const Modal = styled.div`
         .inputBlock{
             width: 90%;
             /* background-color: aqua; */
+            padding-left: 2vw;
             p{
                 text-align: left;
-                margin-left: 10%;
+                /* margin-left: 10%; */
                 margin-bottom: .3rem;
                 font-size: .8rem;
                 /* font-size: .8rem; */
                 font-weight: bold;
             }
             input{
-                width: calc(80% - 0.8rem);
-                height: 30px;
+                width: calc(70% - 0.8rem);
                 background-color: #F4F8FF;
-                border: 1px solid #5EADF7;
+                /* border: 1px solid #5EADF7; */
+                border: none;
                 /* margin: 0; */
-                border-radius: .3rem;
-                padding-left: .8rem;
-
-
+                padding-left: 0.8rem;
+                border-top-left-radius : .3rem;
+                border-bottom-left-radius : .3rem;
             }
-
             input:focus{
-                background-color: #5EADF7;
+                /* background-color: #5EADF7; */
                 outline: none;
-                color: white;
+                /* color: white; */
                 ::placeholder{
                 color: #F4F8FF;
                 }
+            }
+            .regCheckBox{
+                /* background-color: red; */
+                width: 10%;
+                height: 100%;
+                margin: 0, 1px;
+                justify-content: center;
+                align-items: center;
+                display: flex;
+            }
+            .test{
+                display: flex;
+                height: 2rem;
+                background-color: #F4F8FF;
+                width: 80%;
+                justify-content: space-around;
+
+
             }
 
 
         }
         .btnBlock{
-            height: 6rem;
+            height: 30%;
             width: 90%;
             display: flex;
             justify-content: center;
-            align-items: center;
             /* background-color: red; */
 
             button{
-                width: 35%;
+                width: 22%;
                 height: 2.3rem;
                 border-radius: 3rem;
-                margin : 0 6px;
+                margin : 0 1rem;
                 background-color: #050E3D ;
                 border: 1px solid #F4F8FF;
                 color: white;
@@ -94,64 +109,97 @@ const Modal = styled.div`
                 display: flex;
                 justify-content:center;
                 align-items: center;
-                div{
-                    background-color: #6F4F28;
-                    width: 10px;
-                    height: 10px;
-                    border-radius: 5px;
-                    margin-right: 3px;
-                }
 
-            }
-        }
-        .AskBlock{
-            width: 70%;
-            height: 4rem;
-            /* background-color: aqua; */
-            display: flex;
-            justify-content: space-between;
-            align-items: end;
-            p{
-                margin-bottom: .3rem;
-                font-size: .75rem;
-                text-decoration: underline;
-                cursor: pointer;
             }
         }
 `;
 const LeftBox = styled.div`
     width: 50%;
-    height: 100%;
-    background-color: aqua;
+    height: calc(50%-2rem);
+    /* background-color: aqua; */
+    padding-top: 2rem;
+    min-width: 300px;
 `;
 const RightBox = styled.div`
     width: 50%;
-    height: 100%;
-    background-color: red;
+    height: calc(80%-2rem);
+    /* background-color: red; */
+    padding-top: 2rem;
+    min-width: 300px;
+
 `;
 const True = styled.div`
-    width: 3rem;
-    height: 3rem;
-    border: 1.5rem;
+    width: 1.3rem;
+    height: 1.3rem;
+    border-radius: .8rem;
     background-color: #2B5EC2;
+    display: flex;
+    justify-content: center;
+    align-items : center;
+    color : white;
+`;
+const False = styled(True)`
+    background-color: #ED4E4E;
 `;
 
 const SignUpModal = () => {
+    const [inputEmail, setInputEmail] = useState('')
+    const [emailMessage,  setEmailMessage] = useState('')
+    const [isEmail, setIsEmail] =useState(false)
 
+
+    const onChangeEmail = (e) => {
+        setInputEmail(e.target.value)
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailCheck = regexEmail.test(inputEmail)
+        console.log(inputEmail)
+        console.log(emailCheck)
+
+        if (!emailCheck) {
+            setEmailMessage("5자리 이상 12자리 미만으로 입력해 주세요.");
+            setIsEmail(false);    
+        } else {
+            setEmailMessage("올바른 형식 입니다.");
+            setIsEmail(true);
+        }
+    }
     return(
         <Container>
             <Modal>
-                <p className="title">회원가입! </p>
+                <p className="title">회원가입</p>
                 <div className="wrap">
                     <LeftBox>
+                        <div className="inputBlock">
+                            <p>이메일</p>
+                            <div className="test">
+                                <input type="text" placeholder="Email@:DDD.com" value={inputEmail} onChange={onChangeEmail}/>
+                                <div className="regCheckBox">{isEmail ? <True> &#10003; </True> : <False>&times;</False>}</div> 
+                            </div>
+
+                            {/* <p>패스워드</p>
+                            <input type="password" placeholder="Password"/>
+                            <p>패스워드 확인</p>
+                            <input type="password" placeholder="Password"/>
+                            <p>닉네임</p>
+                            <input type="text" placeholder="사용하실 닉네임을 입력해주세요"/>  */}
+                        </div>
 
                     </LeftBox>
-                    <RightBox>
-                        
-                    </RightBox>
+                    {/* <RightBox>
+                        <div className="inputBlock">
+                            <p>이름</p>
+                            <input type="text" placeholder="이름을 입력해주세요"/> 
+                            <p>연락처</p>
+                            <input type="tel" placeholder="연락처를 입력해주세요"/> 
+                            <p>instagram <span style={{fontWeight:'400'}}>(선택사항)</span> </p>
+                            <input type="text" placeholder="instagram ID"/> 
+                        </div>
+                    </RightBox> */}
                 </div>
-
-
+                <div className="btnBlock">
+                    <button>회원가입</button>
+                    <button>돌아가기</button>
+                </div>
             </Modal>
         </Container>
     )

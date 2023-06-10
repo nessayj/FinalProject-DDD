ba9cb7af1da153a107621b1c09fefb70fbf069ef
eunjikwak <eje11@naver.com>
2023-06-10 23:40:44 +0900

    전시리스트페이지(캐러셀 글씨 배치 수정)

--- src/util/Carousel.jsx
+++ src/util/Carousel.jsx
@@ -3,30 +3,54 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import styled from "styled-components";
 
+
 const Container = styled.div`
+  position: relative;
+ .name {
+        color : black;
+        font-weight: bolder;
+        font-size: 2rem;
+        width: 100%;
+        text-align: center;
+        position: absolute;
+        top: 80px;
+        left: 50%;
+        transform: translateX(-50%);
+
+    }
+    .date {
+        font-size: 1.3rem;
+        position: absolute;
+        top: calc(80px + 5rem);
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    .lotation{
+        font-size: 1rem;
+        position: absolute;
+        top: calc(80px + 8rem);
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    @media (max-width: 768px) {
+    .name{
+      font-size: 1.5rem;
+    }
+    .date{
+      font-size : 1rem;
+    }
+
+  }
+`;
+const ImgBox = styled.div`
     width: 100%;
     height: 250px;
     position: relative;
     background-image: url(${props => props.imgUrl});
     background-repeat: no-repeat;
     background-size:cover;
-    opacity: 0.4;
-    .name {
-        color : black;
-        font-weight: bolder;
-        font-size: 2rem;
-        opacity: 1;
-    }
-    .date {
-        font-size: 1rem;
-        opacity: 1;
-    }
-    .lotation{
-        font-size: 1rem;
-        opacity: 1;
-
-
-    }
+    opacity: 0.3;
+   
 
 
 `;
@@ -66,10 +90,11 @@ const Carousel = ({data}) => {
         <div>
           <Slider {...settings}>
             {data.map((e) => (
-                <Container key={e.index} imgUrl ={e.imgUrl}>
+                <Container>
+                <ImgBox key={e.index} imgUrl ={e.imgUrl}/>
                 <div className="name">{e.name}</div>
                 <div className="date">{e.startDate} ~ {e.endDate}</div>
-                <div className="lotation">{e.lotation}</div>
+                <div className="lotation">{e.place}</div>
                 </Container>
             ))}
           </Slider>

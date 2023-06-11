2dc2a903b32795dd8adf851b0241ffdbc845cb73
yunwoo0301 <browniris7@naver.com>
2023-06-11 22:51:54 +0900

    axiosApi 일부 수정

--- src/api/DDDApi.js
+++ src/api/DDDApi.js
@@ -3,7 +3,7 @@ const DDD_DOMAIN = "http://localhost:8111"; // 백엔드에 대한 주소
 
 const DDDApi = {
 
-    // 문의하기리스트 조회
+    // 자유게시판 목록 조회
     boardList: async(freeBoard_No) => { 
         const list = {
             freeBoard_No: freeBoard_No 

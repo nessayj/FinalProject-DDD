c6f5341a5012eda9ec694e0ff9c7a5d717fa7623
YeonJoo <yj.nessa@gmail.com>
2023-06-20 15:18:05 +0900

    관리자페이지 - 오늘의알림, 일자별요약추가

--- src/components/admin/Charts.jsx
+++ src/components/admin/Charts.jsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 
 const ChartContainer = styled.div`
   margin: 0 auto;
-  position: relative;
-  height: 500px;
-  padding-bottom: 50px;
-  border: 1px solid black;
-  
-
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+
 
 const Chart = () => {
   const data = [
@@ -19,93 +19,83 @@ const Chart = () => {
     { date: "2023-06-04", count: 30 },
     { date: "2023-06-05", count: 25 },
     { date: "2023-06-06", count: 12 },
-    // ... 나머지 데이터
+    { date: "2023-06-07", count: 18 },
+    { date: "2023-06-08", count: 5 },
+    { date: "2023-06-09", count: 3 },
+    { date: "2023-06-10", count: 14 },
+    { date: "2023-06-11", count: 9 },
   ];
 
-  const minValue = Math.min(...data.map((item) => item.count));
+  const minValue = 0;
   const maxValue = Math.max(...data.map((item) => item.count));
   const countRange = maxValue - minValue;
 
-  const graphWidth = 400; // 그래프 너비
-  const graphHeight = 300; // 그래프 높이
-  const scaleX = graphWidth / (data.length); // x축
+  const graphWidth = 300; // 그래프 너비
+  const graphHeight = 200; // 그래프 높이
+  const scaleX = graphWidth / (Math.min(data.length, 10)); // x축
   const scaleY = graphHeight / (countRange + 5); // y축
 
+  const recentData = data.slice(-10); // 최근 10일치 데이터 추출
+
   return (
-      <ChartContainer>
-         {/* Y축 구분선과 라벨 */}
-  <svg width="30" height={graphHeight}>
-    {Array.from({ length: Math.ceil(maxValue / 5) }, (_, index) => (index + 1) * 5).map((value) => {
-      const y = graphHeight - (value - minValue) * scaleY;
+    <ChartContainer>
+      {/* Y축 구분선과 라벨 */}
+      <svg width= "30" height={graphHeight+80}>
+        {Array.from({ length: Math.ceil(maxValue / 5) }, (_, index) => (index + 1) * 5).map((value) => {
+          const y = graphHeight - (value - minValue) * scaleY;
 
-      return (
-        <g key={value}>
-          <line x1="0" y1={y} x2="30" y2={y} stroke="gray" strokeWidth={1} />
-          <text x="5" y={y - 5} fill="gray" fontSize="12">
-            {value}명
-          </text>
-        </g>
-      );
-    })}
-  </svg>
-  <svg width={graphWidth} height={graphHeight}>
-    {data.map((item, index) => {
-      const x = index * scaleX;
-      const y = graphHeight - (item.count - minValue) * scaleY;
+          return (
+            <g key={value}>
+              <text x="0" y={y-15} fill="black" fontSize="12">
+                {value}명
+              </text>
+            </g>
+          );
+        })}
+      </svg>
+      {/* 선 그래프 */}
+      <div style={{position: "relative", width: "100%", height: "100%"}}>
+      <svg width={graphWidth} height={graphHeight}>
+        {recentData.map((item, index) => {
+          const x = index * scaleX;
+          const y = graphHeight - (item.count - minValue) * scaleY;
 
-      return (
-        <React.Fragment key={item.date}>
-          <circle
-            cx={x}
-            cy={y}
-            r={4}
-            fill="#050E3D"
-          />
-        </React.Fragment>
-      );
-    })}
-    {data.map((item, index) => {
-      if (index === data.length - 1) return null;
+          return (
+            <React.Fragment key={item.date}>
+              <circle cx={x} cy={y} r={3} fill="#050E3D" />
+              {index !== 0 && (
+                <line
+                  x1={(index - 1) * scaleX}
+                  y1={graphHeight - (recentData[index - 1].count - minValue) * scaleY}
+                  x2={x}
+                  y2={y}
+                  stroke="#050E3D"
+                  strokeWidth={2}
+                />
+              )}
+            </React.Fragment>
+          );
+        })}
+      </svg>
+      {/* X축 라벨 */}
+      <div style={{ position: "absolute", left: 0, width: "100%", display: "flex", justifyContent: "space-between" }}>
+        {recentData.map((item, index) => {
+          const x = index * scaleX;
 
-      const x1 = index * scaleX;
-      const y1 = graphHeight - (item.count - minValue) * scaleY;
-      const x2 = (index + 1) * scaleX;
-      const y2 = graphHeight - (data[index + 1].count - minValue) * scaleY;
-
-      return (
-        <line
-          key={`${item.date}-${data[index + 1].date}`}
-          x1={x1}
-          y1={y1}
-          x2={x2}
-          y2={y2}
-          stroke="#050E3D"
-          strokeWidth={2}
-        />
-      );
-    })}
-    
-    {/* X축 구분선 */}
-    <line x1="0" y1={graphHeight} x2={graphWidth} y2={graphHeight} stroke="gray" strokeWidth={1} />
-  
-  </svg>
-  
-  {/* X축 라벨 */}
-  <div style={{ position: 'relative', marginTop: '10px', marginLeft: '35px' }}>
-    {data.map((item, index) => {
-      const x = index * scaleX;
-
-      return (
-        <React.Fragment key={item.date}>
-          <line x1={x} y1={graphHeight} x2={x} y2={graphHeight + 5} stroke="gray" strokeWidth={1} />
-          <span style={{ position: 'absolute', left: x, top: '100%', transform: 'translateX(-50%)', fontSize: '12px', color: 'black' }}>
-            {item.date.slice(-2)}일
-          </span>
-        </React.Fragment>
-      );
-    })}
-  </div>
-</ChartContainer>
+          return (
+            <React.Fragment key={item.date}>
+              <line x1={x} y1={graphHeight} x2={x} y2={graphHeight + 5} stroke="gray" strokeWidth={1} />
+              <span
+                style={{ position: "absolute", left: x+20, top: "100%", transform: "translateX(-50%)", fontSize: "12px", color: "black" }}
+              >
+                {item.date.slice(-2)}일
+              </span>
+            </React.Fragment>
+          );
+        })}
+      </div>
+      </div>
+    </ChartContainer>
   );
 };
 
--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -9,10 +9,80 @@ const DashboardContainer = styled.div`
         margin-left: 1rem;
         text-decoration: underline;
     }
+    .container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    }
+    .today-noti{
+        background-color: #e3e3e3;
+        width: 80%;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        >h4{
+            margin-top: .5rem;
+            margin-bottom: 0;
+        }
+
+        .noti-list{
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            gap: 1rem;
+            >p {
+                font-size: 0.8em;
+            }
+        }
+    }
+    .second-row{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
     .charts{
         margin-left: 1rem;
+        margin-top: 1rem;
+        width: 23em;
+        height: 20em;
+        text-align: center;
+        background-color: #e3e3e3;
+        border-radius: 2rem;
 
     }
+    .dailyData{
+
+        width: 23em;
+        height: 20em;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border-radius: 2rem;
+        background-color: #e3e3e3;
+        margin-left: 1rem;
+        margin-top: 1rem;
+        table{
+            width: 20rem;
+            height: 100%;
+            font-size: 0.7em;
+            text-align: center;
+            border-collapse: collapse;
+            border-spacing:0;
+        th {
+            background-color: #050E3D;
+            margin: 0;
+            color: white;
+            border: none;
+        }
+
+        td, th {
+            border: none;
+        }
+
+    }
+    }
+
+
 
 
 
@@ -23,15 +93,82 @@ const DashboardContainer = styled.div`
 
 
 const DashBoard = () => {
+    // 일자별 요약 테이블 임시데이터
+    const data = [
+        {
+          date: "2023-06-01",
+          visitors: 10,
+          newMembers: 5,
+          exhibitions: 3,
+          bookings: 7,
+          posts: 12,
+        },
+        {
+            date: "2023-06-03",
+            visitors: 102,
+            newMembers: 53,
+            exhibitions: 3,
+            bookings: 10,
+            posts: 21,
+          },
+      ];
+    
+      const recentData = data.slice(-10).reverse(); // 최신순으로 정렬된 최근 10개 데이터 추출
+    
 
 
   return(
       <DashboardContainer>
         <h3 className="dash-title">Dash Board</h3>
+        <div className="container">
+        <div className="today-noti">
+            <h4>오늘의 알림</h4>
+            <hr />
+            <div className="noti-list">
+            <p>신규회원 : </p>
+            <p>탈퇴회원 : </p>
+            <p>전시 중 : </p>
+            <p>예매완료 : </p>
+            <p>게시글 수 : </p>
+            </div>
+        </div>
+        <div className="second-row">
         <div className="charts">
+            <h4>이번달 가입현황</h4>
             <Chart/>
         </div>
-          
+        <div className="dailyData">
+            <h4>일자별 요약</h4>
+            <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                    <th style={{ width: "20%" }}>일자</th>
+                    <th style={{ width: "16%" }}>방문자</th>
+                    <th style={{ width: "16%" }}>가입수</th>
+                    <th style={{ width: "16%" }}>전시 중</th>
+                    <th style={{ width: "16%" }}>예매완료</th>
+                    <th style={{ width: "16%" }}>게시글 수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recentData.map((item) => (
+                    <tr key={item.date}>
+                        <td>{item.date.slice(5)}</td>
+                        <td>{item.visitors}</td>
+                        <td>{item.newMembers}</td>
+                        <td>{item.exhibitions}</td>
+                        <td>{item.bookings}</td>
+                        <td>{item.posts}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+
+        </div>
+        </div>
+        </div>
 
       </DashboardContainer>
   )
